08b104cec719ec3231fa87da5818c09f92b738d2
zapxcero <zapxcero@gmail.com>
2022-03-21 13:33:11 +0800

    Cleaned the code

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,79 +1,72 @@
 # frozen_string_literal: true
 
+# Plays a game of Hangman
 class Hangman
-  attr_accessor :dictionary, :secret_word, :guess
-
   def initialize
     @guess = 10
     @game_running = true
-    @dictionary = load_dictionary
-    @secret_word = random_dictionary
+    @dict = load_dict
+    @secret_word = select_secret_word
     p @secret_word
     @user_guesses = []
     game
   end
 
-  def load_dictionary
+  def load_dict
     File.open('dictionary.txt').readlines
   end
 
-  def random_dictionary
-    sample_word = @dictionary.sample.chomp
-    if sample_word.length >= 5 && sample_word.length <= 12
-      sample_word
+  def select_secret_word
+    random_word = @dict.sample.chomp
+    if random_word.length >= 5 && random_word.length <= 12
+      random_word
     else
-      random_dictionary
+      select_secret_word
     end
   end
 
   def hint_secret_word
-    puts "Here's the secret word's length and the guesses you've made."
-    secret_word.chars.each do |letter|
+    puts "\nHere's the secret word's length and the guesses you've made."
+    puts "\nGuesses: #{@user_guesses.join(', ')}\n\n"
+    @secret_word.chars.each do |letter|
       if @user_guesses.include? letter
-        print " #{letter} "
+        print "#{letter} "
       else
-        print ' _ '
+        print '_ '
       end
     end
     print "\n"
   end
 
-  def ask_user_guess
-    print "\n\nEnter a letter to guess the secret word: "
-    letter = gets.chomp
+  def user_guess_letter
+    print "\n\nEnter a letter to (#{@guess}) guess the secret word: "
+    letter = gets.chomp[0]
     @user_guesses.push(letter) unless @user_guesses.include? letter
-    winner?
   end
 
   def winner?
-    puts "Winner method"
+    pass
+  end
+
+  def main_menu
+    puts "Do you want to:\n\n[1]Start a new game\n[2]Resume a saved game?\n\n"
+    print 'Enter choice: '
+    gets.chomp.to_i
   end
 
   def game
-    puts "Do you want to:\n\n[1]Start a new game\n[2]Resume a saved game?\n\n"
-    print 'Enter choice: '
-    choice = gets.chomp
-
-    case choice.to_i
+    case main_menu
     when 1
       loop do
         hint_secret_word
-        ask_user_guess
-        # ask user to guess
-        # if user guess X char, then add it to guesses_he_made
-        #
-        # to determine if user guessed all words
-        # check if @secret_word.chars.includes? @user_guesses. maybe something like adding or subtracting arrays
+        user_guess_letter
         @guess -= 1
-        break if @guess.zero?
-        break if @game_running == false
-        # break if guesses is out or game is over
+        break if @guess.zero? || @game_running == false
       end
     when 2
-      # Saved Game
-      pass
+      puts "Saved Game!"
     end
   end
 end
 
-hang = Hangman.new
+Hangman.new
